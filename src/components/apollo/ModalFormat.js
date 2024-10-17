20b5db76604f4576bf1962de8514dba334da6818
Michele Marcucci <michele@befree.it>
2024-10-17 15:19:50 +0200

    Added formatProgress request

--- src/components/apollo/ModalFormat.js
+++ src/components/apollo/ModalFormat.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
   Button,
   Modal,
@@ -10,13 +11,47 @@ import {
   Flex,
   Text,
 } from '@chakra-ui/react';
+import { NODE_FORMAT_PROGRESS_QUERY } from '../../graphql/node';
+import { useQuery } from '@apollo/client';
+
 const ModalFormat = ({ isOpen, onClose, onFormat }) => {
+  const [updateInProgress, setUpdateInProgress] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [done, setDone] = useState(false);
+
+  const {
+    data: dataProgress,
+    startPolling: startPollingProgress,
+    stopPolling: stopPollingProgress,
+  } = useQuery(NODE_FORMAT_PROGRESS_QUERY);
+
+  const startFormat = () => {
+    onFormat();
+    setUpdateInProgress(true);
+    startPollingProgress(3000);
+  };
+
+  const { value: remoteProgress } =
+    dataProgress?.Node?.formatProgress?.result || {};
+
+  useEffect(() => {
+    if (updateInProgress) {
+      setProgress(remoteProgress);
+    }
+
+    if (remoteProgress >= 90 || !isOpen) {
+      stopPollingProgress();
+      setUpdateInProgress(false);
+      setProgress(0);
+      setDone(true);
+    }
+  }, [updateInProgress, remoteProgress, isOpen, stopPollingProgress]);
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Format Node SSD disk</ModalHeader>
-        <ModalCloseButton />
         <ModalBody>
           <Flex>
             <Text>
@@ -24,13 +59,28 @@ const ModalFormat = ({ isOpen, onClose, onFormat }) => {
               data.
             </Text>
           </Flex>
+          {updateInProgress && (
+            <Flex>
+              <Text>Formatting in progress... {progress}% complete</Text>
+            </Flex>
+          )}
         </ModalBody>
 
         <ModalFooter>
-          <Button variant="ghost" mr={3} onClick={onClose}>
+          <Button
+            variant="ghost"
+            mr={3}
+            onClick={onClose}
+            isDisabled={updateInProgress}
+          >
             Close
           </Button>
-          <Button colorScheme="red" onClick={onFormat}>
+          <Button
+            colorScheme="red"
+            onClick={startFormat}
+            isDisabled={updateInProgress}
+            isLoading={updateInProgress}
+          >
             YES, Format it
           </Button>
         </ModalFooter>
