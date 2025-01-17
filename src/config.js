a4630bc63d5eb92529f854ff83c31ab86f887aa0
Michele Marcucci <michele@befree.it>
2025-01-17 18:35:59 +0100

    Better threshold based on Apollo 2

--- src/config.js
+++ src/config.js
@@ -18,8 +18,8 @@ const config = {
 		},
 	],
 	thresholds: {
-		MINER_STOP_PENDING_THRESHOLD: 5, // Seconds to wait for stopping
-		MINER_START_PENDING_THRESHOLD: 30, // Seconds to wait for starting
+		MINER_STOP_PENDING_THRESHOLD: 10, // Seconds to wait for stopping
+		MINER_START_PENDING_THRESHOLD: 40, // Seconds to wait for starting
 		MINER_SUCCESS_ALERT_DURATION: 5000, // Milliseconds to display success alert
 		MINER_SUCCESS_THRESHOLD: 60, // Seconds to show success alert
 	},
